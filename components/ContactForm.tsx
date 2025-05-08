a4218009e81e7c5b568fd5724a418fe8fdf0037e
Mishael Wilcox <81692374+thefoxclan@users.noreply.github.com>
2025-05-08 10:42:02 -0400

    name, phone number update

--- components/ContactForm.tsx
+++ components/ContactForm.tsx
@@ -1,9 +1,12 @@
 "use client";
-import React from "react";
+import React, { useState } from "react";
 import { useForm, ValidationError } from "@formspree/react";
+import PhoneInput from "react-phone-input-2";
 
 export function ContactForm() {
   const [state, handleSubmit] = useForm("manojqzo");
+  const [phone, setPhone] = useState("");
+
   if (state.succeeded) {
     return (
       <p className="text-center text-primary font-semibold">
@@ -13,6 +16,44 @@ export function ContactForm() {
   }
   return (
     <form onSubmit={handleSubmit} className="space-y-4">
+      <div className="space-y-2">
+        <label htmlFor="name" className="text-sm font-medium">
+          Name
+        </label>
+        <input
+          id="name"
+          type="text"
+          name="name"
+          required
+          placeholder="Enter your name"
+          className="flex h-10 w-full rounded-md border border-input px-3 py-2 text-sm focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-primary focus-visible:ring-offset-2"
+        />
+        <ValidationError prefix="Name" field="name" errors={state.errors} />
+      </div>
+
+      <div className="space-y-2">
+        <label htmlFor="phone" className="text-sm font-medium">
+          Phone Number
+        </label>
+        <PhoneInput
+          country="ca"
+          value={phone}
+          onChange={setPhone}
+          inputProps={{
+            name: "phone",
+            required: true,
+            id: "phone",
+            placeholder: "Enter your phone number",
+          }}
+          containerClass="w-full border border-input rounded-md"
+          containerStyle={{ width: "100%" }}
+          inputClass="h-10 w-full rounded-md px-3 py-2 text-sm focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-primary focus-visible:ring-offset-2"
+          inputStyle={{ width: "100%", borderWidth: 0 }}
+          buttonStyle={{ borderWidth: 0 }}
+        />
+        <ValidationError prefix="Phone" field="phone" errors={state.errors} />
+      </div>
+
       <div className="space-y-2">
         <label htmlFor="email" className="text-sm font-medium">
           Email Address
